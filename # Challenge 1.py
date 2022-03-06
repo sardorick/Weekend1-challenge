61d2fea69bc274ffaff5279a6837c37f31ca5f71
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-03-06 19:57:37 +0100

    4 Challenges

---
+++ # Challenge 1.py
@@ -0,0 +1,11 @@
+# Challenge 1
+# Create a function that receives a number print out this pyramid
+
+def print_pyramid(num):
+    i = 1
+    while i <= num:
+        print('#')
+        i += 1
+    print("End")
+
+print(print_pyramid(5))        
